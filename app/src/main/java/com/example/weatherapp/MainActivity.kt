8deccc446db80715d6ac1cd6b6622b9daadc2351
Albert-Byrone <albertbyrone1677@gmail.com>
2021-08-25 22:37:59 +0300

    minor

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.weatherapp
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+
 
 val CITY : String = "Nairobi, ke"
 val API: String = "b31ef167a3ff9a07823ccf76fc32a2d8"
@@ -10,5 +14,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        weatherTask().execute()
+    }
+
+    inner class weatherTask : AsyncTask<String, Void, String>()
+    {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
+
+        }
     }
 }
