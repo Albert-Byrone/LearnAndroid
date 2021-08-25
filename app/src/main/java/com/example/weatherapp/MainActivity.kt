a543da693bc7d80e0ebff394584657b842f9bcbf
Albert-Byrone <albertbyrone1677@gmail.com>
2021-08-25 22:37:48 +0300

    add placeholders to store the api key and th city

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+val CITY : String = "Nairobi, ke"
+val API: String = "b31ef167a3ff9a07823ccf76fc32a2d8"
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
